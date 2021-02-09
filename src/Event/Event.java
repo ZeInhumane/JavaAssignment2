c0b91087c5caa26dcc66b2c7370b4501e424ae0f
ZeInhumane <69852169+ZeInhumane@users.noreply.github.com>
2021-02-09 16:30:17 +0800

    Push

--- src/Event/Event.java
+++ src/Event/Event.java
@@ -1,52 +1,99 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package Event;
 
 /**
- *
- * @author HP
+ * The Event class provides a basic encapsulation of the 4 
+ * properties that defines an event, and its necessary getters and setters.
+ * @author Alvin Tang
  */
 public class Event {
-
-    /**
-     * @param args the command line arguments
-     */
-    private String name, organizer, date;
+    private String name;
+    private String organiser;
+    private String date;
     private double fees;
-//    name, organizer, fees and date and time of the event
-    public Event(String inName,String inOrganizer,String inDate,
-    double inFees){
-    this.name = inName;
-    this.organizer = inOrganizer;
-    this.date = inDate;
-    this.fees = inFees;
-}
-    public void setName(String inName){
-        name = inName;
-    }
-    public void setOrganizer(String inOrganizer){
-        organizer = inOrganizer;
-    }
-    public void setDate(String inDate){
-        date = inDate;
-    }
-    public void setName(double inFees){
-        this.fees = inFees;
-    }
-    public String getName(){
-        return name;
-    }
-    public String getOrganizer(){
-        return organizer;
-    }
-    public String getDate(){
-        return date;
-    }
-    public double fees(){
-        return fees;
+    
+    /**
+     * Constructor
+     * @param inName A string to describe the name of the event.
+     * @param inOrganiser A string to describe the organiser.
+     * @param inDate A string representing the date.
+     * @param inFees A double for the cost of the event.
+     */
+    public Event(String inName,
+            String inOrganiser,
+            String inDate,
+            double inFees){
+
+        // Complete constructor here.
     }
     
+    /**
+     * Getter for name.
+     * @return name of event
+     */
+    public String getName(){
+        // TODO
+    }
+    /**
+     * Changes the name of the event.
+     * @param inName 
+     */
+    public void setName(String inName){
+        // TODO
+    }
+    /**
+     * Returns the organiser for the event.
+     * @return organiser of event.
+     */
+    public String getOrganiser(){
+        // TODO
+    }
+    
+    /**
+     * Change organiser for the event.
+     * @param inOrganiser 
+     */
+    public void setOrganiser(String inOrganiser){
+        // TODO
+    }
+
+    /**
+     * Returns date of event as a string.
+     * @return Date of event.
+     */
+    public String getDate() {
+        // TODO
+    }
+    
+    /**
+     * Change the date for the event.
+     * @param inDate 
+     */
+    public void setDate(String inDate) {
+        // TODO
+    }
+    
+    /**
+     * Returns cost of the event.
+     * @return Cost of event.
+     */
+    public double getFees(){
+        // TODO
+    }
+    /**
+     * Change cost of event.
+     * @param inFees 
+     */
+    public void setFees(double inFees){
+        // TODO
+    }
+    
+    /**
+     * Test program for the class.
+     * @param args 
+     */
+    public static void main(String[] args) {
+        Event theEvent = new Event("Swimming", "SP", "20/2/2021", 20.99);
+        System.out.println(theEvent.getName() + " is on " + theEvent.getDate());
+    }
+
 }
--- src/Event/EventManagement.java
+++ src/Event/EventManagement.java
@@ -1,14 +1,155 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package Event;
 
+import javax.swing.JOptionPane;
 /**
- *
- * @author HP
+ * Collection of events. Implemented here using a one-d array.
+ * @author Alvin Tang
  */
 public class EventManagement {
     
+    private Event[] events; /// All available events
+    
+    private int countSearchResult; /// used by searchByFees
+    /**
+     * 
+     * @return number of items in the returned search array.
+     */
+    public int getCountSearchResult(){
+        return 0;
+        // TODO
+    }
+    
+    /**
+     * Constructor to initialize the events in the array.
+     */
+    public EventManagement(){
+        // TODO
+    }
+
+    /**
+     * Show all events using a Message Dialog.
+     * This may be changed to return a string instead of displaying a dialog.
+     */
+    public void displayAllEvents(){
+        // Best effort at even spacing for the data presented.
+        // TODO
+        
+        // loop thru all items in the array and output details.
+        // TODO
+
+        // Change to return as string instead if desired.
+        // TODO
+    }
+    
+    /**
+     * Add new event to the events array.
+     * @param inName
+     * @param inOrganiser
+     * @param inDate
+     * @param inFees 
+     */
+    public void addEvent(String inName, String inOrganiser, String inDate, double inFees) {
+        // Create new array with current array length + 1
+        // TODO
+        
+        // copy existing events to the new array
+        // TODO
+        
+        // the new event is appended as the last event in new array
+        // TODO
+        
+        // Reassign events to the new events.  The old array will be discarded.
+        // TODO
+    }
+    
+
+    /**
+     * Delete an event on a given name. 
+     * Capitalization doesn't matter. Uses the utility method, searchByName()
+     * @param inEventName
+     */
+    public void deleteEvent(String inEventName) {
+        // Try to locate the event by name first
+        // TODO
+        
+        // event not found
+        // TODO
+        
+        else {
+            // create the new array with events less the deleted event
+            // Size of new array will be current array length, less one
+            // TODO
+
+            // there are 2 indices we'll be working with.
+            // i is the index for the current array
+            // theNewIndex is the index into theNewList
+            // TODO
+                // this isn't the droid we're looking for...
+                // We'll add all events that are not the event we want to delete
+                // TODO
+            }
+            
+            // TODO
+        }
+    }
+
+    /**
+     * Utility method that can be used by itself, or by the delete method.
+     * @param inEventName
+     * @return Event object, if found.
+     */
+//    public Event searchByName(String inEventName) {
+//        //TODO
+//            // Found it! Retrieve. 
+//            // Assuming there are no repeats.
+//            // If there are repeats, this will retrieve the last one found.
+//            // TODO
+//        
+//        // TODO
+//    }
+    
+    /**
+     * search for all events equal or less than inFees
+     * @param inMaxFees
+     * @return Returns array containing events that meet the search criteria.
+     */
+//    public Event[] searchByFees(double inMaxFees) {
+//        // TODO
+//        // Max case scenario is that all events meet inMaxFees
+//        // TODO
+//        
+//        // iterate all items in the events array
+//        // TODO
+//        
+//        // TODO
+//    }
+
+    /**
+     * Test method for all the methods in EventManager Class.
+     * @param args
+     */    
+//    public static void main(String[] args) {
+//        EventManager theEM = new EventManager();
+//        theEM.displayAllEvents();
+//        theEM.addEvent("Kickboxing", "Alvin", "5/6/2099", 59);
+//        
+//        theEM.displayAllEvents();
+//        
+//        // Test Search by name
+//        Event theFoundEvent = theEM.searchByName("Swimming");
+//        if (theFoundEvent == null)
+//           JOptionPane.showMessageDialog(null, "Swimming not found!");
+//        theFoundEvent = theEM.searchByName("Dancing");
+//        
+//        if(theFoundEvent != null)
+//            JOptionPane.showMessageDialog(null, theFoundEvent.getName() + " is by " + theFoundEvent.getOrganiser());
+//        // end Test Search by name
+//        
+//        Event[] theResults = theEM.searchByFees(11);
+//        for(int i = 0; i< theEM.getCountSearchResult(); i++)
+//            System.out.println(theResults[i].getName() + " costing $" + theResults[i].getFees());
+//        
+//        theEM.deleteEvent("Dancing");
+//        theEM.displayAllEvents();
+//    }
 }
